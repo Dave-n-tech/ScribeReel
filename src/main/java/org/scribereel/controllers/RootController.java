d22193f0f628c7e4103be631150d446fbd2a7948
Dave-n-tech <davefast2003@gmail.com>
2026-09-09 00:18:14 +0100

    chore: add RootController for home "/" endpoint

---
+++ src/main/java/org/scribereel/controllers/RootController.java
@@ -0,0 +1,12 @@
+package org.scribereel.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String home() {
+        return "ScribeReel Backend API is running successfully!";
+    }
+}
